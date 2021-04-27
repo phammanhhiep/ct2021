ad506c7170d83d3e2e7ff997a52024d610f8515c
hieppm <hieppm.work@gmail.com>
2021-04-27 12:11:52 +0700

    Implement MultiScaleGanLoss

--- src/models/networks/loss.py
+++ src/models/networks/loss.py
@@ -12,27 +12,51 @@ class AEINetLoss(nn.Module):
     def __init__(self, opt): pass
 
 
-class GanLoss(nn.Module):
+class MultiScaleGanLoss(nn.Module):
     def __init__(self):
         super().__init__()
 
 
-    def forward(self, multi_y, t): pass
+    #TODO: review the implementation of the hinge loss in project https://trello.com/c/cA9SYd0x. There are some complications which are not handled in here. Also, the implementation may be
+    #TODO: verify size of input y 
+    def forward(self, y, t, compute_d_loss=True):
+        """Reshape the input and compute loss for individual scales first, and
+        then sum up the losses. 
+        
+        Args:
+            y (TYPE): a batch of multi-scale discriminator outputs, each of
+            which is a list of 3D tensors; size of y is expected to be
+            (batch_size, multi_scale_size, discriminator_output) 
+            t (TYPE): 1 for real image; -1 for fake image
+            compute_d_loss (bool, optional): compute d loss or g loss
+        
+        Returns:
+            TYPE: Description
+        """ 
+        y = torch.reshape(y, (1, 0, 2))
+        loss = torch.zeros(y.size()[1])
+        for yi in y:
+            loss += self._hinge_loss(yi, t, compute_d_loss)
+        return loss
 
 
-
-    def _hinge_loss(self, multi_y, t):
+    def _hinge_loss(self, y, t=1, compute_d_loss=True):
         """Summation of individual hinge losses.
         
         Args:
-            multi_y (TYPE): prediction
-            t (TYPE): 1=real, and 0=synthesis
+            y (TYPE): a batch of outputs
+            t (bool, optional): 1=real, -1=fake 
+            compute_d_loss (bool, optional): compute loss for d or g.
         """
+        loss = 0
+        if compute_d_loss:
+            loss = -torch.mean(
+                torch.min(self.zeros(y.size()), -1 + t * y), 
+                dim=0)
+        else:
+            loss = -torh.mean(y, dim=0)
+        return loss
 
-
-    def _feature_match_loss(self):
-        """Summary
-        """
 
 class AttrLoss(nn.Module):
 
