9f0b23b8dfbbbc04c910e2a8fbafb2b193ede55f
hieppm <hieppm.work@gmail.com>
2021-04-28 17:12:47 +0700

    Modify layers of PatchGANDiscriminator, and remove _update_option method in MultiScaleDiscriminator
    In PatchGANDiscriminator model, add a spectral norm  to every conv layer, share kernel size for all conv layer, add signmoid after the last layer, include leakyReLU slope option, recorrect the forward method

--- src/models/networks/discriminator.py
+++ src/models/networks/discriminator.py
@@ -5,39 +5,21 @@ class MultiScaleDiscriminator(nn.Module):
     def __init__(self, opt):
         super().__init__()
         self.opt = opt
-        self.num_ds = opt["multi_scale_d"]["num_ds"]
-
+        self.num_ds = opt["num_ds"]
+        self.ds_scale_factors = opt["downsample_scale_factors"]
         self.ds = []
+
         for n in range(self.num_ds):
-            self.ds.append(PatchGANDiscriminator(
-                opt["multi_scale_d"]["patch_gan"]))
-            self._update_opt(n)
+            self.ds.append(PatchGANDiscriminator(opt["patch_gan_d"]))
 
 
     def forward(self, x):
         y = []
         for n in range(self.num_ds):
+            x = nn.functional.interpolate(x, 
+                (1, 1, self.ds_scale_factors[n], self.ds_scale_factors[n]))
             y.append(self.ds[n](x))
-            self._downsample(x, n)
         return y
-
-
-    def _downsample(self, x, d_index):
-        """Downsample the input x, with parameters defined in self.opt. The 
-        options are determined by the d_index. 
-        
-        Args:
-            x (TYPE): Description
-            d_index (TYPE): Description
-        """
-        
-
-    def _update_opt(self, d_index):
-        """Update the options corresponding to the d_index
-        
-        Args:
-            d_index (TYPE): index of the target discriminator
-        """
 
 
 class PatchGANDiscriminator(nn.Module):
@@ -46,41 +28,43 @@ class PatchGANDiscriminator(nn.Module):
     translation with conditional adversarial networks"
     """
 
+    #TODO: verify if spectral norm is used at the same time with instance norm, or instance norm is replaced by spectral norm
+    #TODO: include an option for spectral norms
     def __init__(self, opt):
         super().__init__()
-        self.opt = 
-        self.model = []
+        conv_blk_opt = opt["conv_blk"]
+        in_channels = conv_blk_opt["conv"]["in_channels"]
+        out_channels = conv_blk_opt["conv"]["out_channels"]
+        kernel_size = conv_blk_opt["conv"]["out_channels"]
+        leaky_relu_slope = conv_blk_opt["leaky_relu"]["slope"]
+        innorm_num_features = conv_blk_opt["innorm"]["num_features"]
         num_blk = opt["num_blk"]
+        self.model = []
 
         for n in range(num_blk):
             if n == 0:
                 self.model += [
-                    nn.Conv2d(opt["conv"]["in_channels"][n],
-                        opt["conv"]["out_channels"][n]
-                        opt["conv"]["kernel_size"][n]
-                        ),
-                    nn.LeakyReLU()
+                    nn.utils.spectral_norm(nn.Conv2d(
+                        in_channels[n], out_channels[n], kernel_size)),
+                    nn.LeakyReLU(leaky_relu_slope)
                     ]
             else:
                 self.model += [
-                    nn.Conv2d(opt["conv"]["in_channels"][n],
-                        opt["conv"]["out_channels"][n]
-                        opt["conv"]["kernel_size"][n]),
-                    nn.InstanceNorm2d(
-                        opt["innorm"]["num_features"][n]),
-                    nn.LeakyReLU()
+                    nn.utils.spectral_norm(nn.Conv2d(
+                        in_channels[n], out_channels[n], kernel_size)),
+                    nn.InstanceNorm2d(innorm_num_features[n]),
+                    nn.LeakyReLU(leaky_relu_slope)
                     ]
-        self.model += nn.Conv2d(
-            opt["last_layer"]["in_channels"],
-            opt["last_layer"]["out_channels"][n]
-            opt["last_layer"]["kernel_size"][n]            
-            )
+        self.model += [
+            nn.utils.spectral_norm(nn.Conv2d(
+                opt["last_layer"]["in_channels"],
+                opt["last_layer"]["out_channels"]
+                opt["last_layer"]["kernel_size"]            
+                )),
+            nn.Sigmoid()
+            ]
         self.model = nn.sequential(self.model)
 
 
     def forward(self, x):
-        h = x
-        for subnet in self.model:
-            h = subnet(x)
-            x = h
-        return h   
+        return self.model(x)
