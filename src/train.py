22132de64b2009b42e6ee424dafe9a6188071e82
hieppm <hieppm.work@gmail.com>
2021-05-01 22:26:55 +0700

    Refactor according to changes in options

--- src/train.py
+++ src/train.py
@@ -4,35 +4,42 @@ import logging
 from torch.utils import data
 
 
-from common.utils import create_root_logger
-from common.train_options import TrainOptions
+from options.options import Options
 from trainers.faceShifter_trainer import FaceShifterTrainer
-from data.dataset import Dataset
+from data.dataset import FFHQ, CelebAHQ
 
 
-#TODO: remove the need to hard-code a trainer, but select a trainer through options. Consider using trainer from pytorch_lightning
-def train(logger):
-    opt = TrainOptions()
+#TODO: remove the need to hard-code using if-else statement and specify datasets and trainers. Review SPADE project a hint. 
+#TODO: Consider using trainer from pytorch_lightning
+def train():
+    opt = Options()
+    dataset_name = opt["data"]["train"]
+    trainer_name = opt["trainer"]["name"] 
 
-    dataset = Dataset(opt["dataset"]["root_dir"], 
-        opt["dataset"]["data_list_file"])
+    if dataset_name == "FFHQ":
+        train_datasets = FFHQ(opt[dataset_name]["train"])
+    elif dataset_name == "CelebAHQ":
+        train_datasets = CelebAHQ(opt[dataset_name]["train"])
     
-    dataloader = data.DataLoader(dataset, 
-        batch_size=opt["batch_size"],
+    dataloader = data.DataLoader(
+        train_datasets, 
+        batch_size=opt["data"]["batch_size"],
         shuffle=True,
-        num_workers=opt["dataset"]["num_worker"],
+        num_workers=opt["data"]["num_worker"],
         )
 
-    trainer = FaceShifterTrainer(opt["trainer"]) #TODO: Review the argument
+    if trainer_name == "FaceShifterTrainer"
+        trainer = FaceShifterTrainer(opt[trainer_name])
 
-    for epoch in range(opt["epochs"]):
+    for epoch in range(opt["trainer"]["epochs"]):
         for j, batch_data in enumerate(dataloader):
             trainer.fit(batch_data, j)
         trainer.update_optimizer(epoch)
 
-        if epoch % opt["log"]["checkpoint_interval"]
-            trainer.save_checkpoint(epoch, opt["log"]["checkpoint_dir"])
+        if epoch % opt["checkpoint"]["save_interval"]
+            trainer.save_checkpoint(epoch, opt["checkpoint"]["save_dir"])
 
  
 if __name__ == "__main__":
-    train(create_root_logger())
+    create_root_logger()
+    train()
