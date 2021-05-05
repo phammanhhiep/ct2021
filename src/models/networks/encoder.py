628dc6fd68a8d6a05bf48db9e433c09179005eb7
hieppm <hieppm.work@gmail.com>
2021-05-05 17:06:30 +0700

    Correct AttrEncoder.__init__ and AttrEncoder.forward
    Set in_channels of Convtranspose2D deccoder_in_channel_list in AttrEncoder.__init__
    Remove AttrEncoder.post_decoder_forward
    Include the last output of encoder in self.decoder_features in AttrEncoder.forward

--- src/models/networks/encoder.py
+++ src/models/networks/encoder.py
@@ -23,20 +23,21 @@ class AttrEncoder(nn.Module):
         in_channels = opt["in_channels"]
         eopt = opt["encoder"]
         dopt = opt["decoder"]
-        leakyReLU_slope = opt["leakyReLU_slope"]
+        LeakyReLU_slope = opt["LeakyReLU_slope"]
 
-        encoder_out_channel_list = self.eopt["conv"]["out_channel_list"] 
-        conv_kernel_size = self.eopt["conv"]["kernel_size"]
-        conv_stride = self.eopt["conv"]["stride"]
-        conv_padding = self.eopt["conv"]["padding"]
+        encoder_out_channel_list = eopt["conv"]["out_channel_list"] 
+        conv_kernel_size = eopt["conv"]["kernel_size"]
+        conv_stride = eopt["conv"]["stride"]
+        conv_padding = eopt["conv"]["padding"]
 
-        decoder_out_channel_list = self.dopt["conv"]["out_channel_list"]
-        conv_tr_kernel_size = self.dopt["conv_tr"]["kernel_size"]
-        conv_tr_stride = self.dopt["conv_tr"]["stride"]
-        conv_tr_padding = self.dopt["conv_tr"]["padding"]
+        deccoder_in_channel_list = dopt["conv_tr"]["in_channel_list"]
+        decoder_out_channel_list = dopt["conv_tr"]["out_channel_list"]
+        conv_tr_kernel_size = dopt["conv_tr"]["kernel_size"]
+        conv_tr_stride = dopt["conv_tr"]["stride"]
+        conv_tr_padding = dopt["conv_tr"]["padding"]
 
-        self.ebn = self.eopt["num_blks"]
-        self.dbn = self.dopt["num_blks"]
+        self.ebn = eopt["num_blks"]
+        self.dbn = dopt["num_blks"]
         self.decoder_features = []
         self.encoder_features = []
         self.encoder = []
@@ -45,36 +46,33 @@ class AttrEncoder(nn.Module):
 
         for n in range(self.ebn):
             out_channels = encoder_out_channel_list[n]
-            sq = nn.sequential(
-                nn.Conv2d(
-                    in_channels, out_channels, conv_kernel_size, conv_stride, 
-                    conv_padding),
+            sq = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, conv_kernel_size, 
+                    conv_stride, conv_padding),
                 nn.BatchNorm2d(out_channels),
-                nn.LeakyReLU(leakyReLU_slope)
+                nn.LeakyReLU(LeakyReLU_slope)
                 )
             in_channels = out_channels
-            sq.register_forward_hook(encoder_forward_hook)
+            sq.register_forward_hook(self.encoder_forward_hook)
             self.encoder.append(sq)
 
-
-        self.encoder = nn.sequential(self.encoder)
+        self.encoder = nn.Sequential(*self.encoder)
 
         for n in range(self.dbn):
-            in_channels = out_channels
             out_channels = decoder_out_channel_list[n]
-            self.decoder.append(nn.sequential(
-                nn.ConvTranspose2d(
-                    in_channels, out_channels, conv_tr_kernel_size, 
-                    conv_tr_stride, conv_tr_padding),
+            in_channels = deccoder_in_channel_list[n]
+            self.decoder.append(nn.Sequential(
+                nn.ConvTranspose2d(in_channels, out_channels, 
+                    conv_tr_kernel_size, conv_tr_stride, conv_tr_padding),
                 nn.BatchNorm2d(out_channels),
-                nn.LeakyReLU(leakyReLU_slope)   
+                nn.LeakyReLU(LeakyReLU_slope)   
                 ))
 
     def pre_forward(self):
         self.decoder_features = []
 
 
-    def encoder_forward_hook(self, x, y):
+    def encoder_forward_hook(self, module, x, y):
         """Append the encoder layer output to a list.
         
         Args:
@@ -83,40 +81,24 @@ class AttrEncoder(nn.Module):
         self.encoder_features.append(y)
 
 
-    def decoder_post_forward(self, x1, n=None):
-        """Does two things
-        If n is provided, get the corresponding encoder output and concatenate
-        the decoder and encoder layer outputs.
-        Append the decoder layer output to a list.
-        
-        Args:
-            x1 (TYPE): output of the target decoder layer.
-            n (None, optional): index of the target decoder layer.
-        
-        Returns:
-            TYPE: Description
-        """
-        x = x1
-        if n is not None:
-            x = torch.cat([x1, self.encoder_features[-2 - n]], dim=1)
-        self.decoder_features.append(x)
-        return x
-
-
     def forward(self, x):
         self.pre_forward()
-        x = self.encoder[n](x)
+        x = self.encoder(x)
+        self.decoder_features.append(x)
 
         for n in range(self.dbn):
             x = self.decoder[n](x)
-            x = self.decoder_post_forward(x, n)
+            x = torch.cat([x, self.encoder_features[-2 - n]], dim=1)
+            self.decoder_features.append(x)
 
-        x = nn.functional.interpolate(x, size=2, mode='bilinear', 
+        x = nn.functional.interpolate(x, scale_factor=(2,2), mode='bilinear', 
             align_corners=True)
-        return self.decoder_post_forward(x)
+        self.decoder_features.append(x)
+        
+        return x
 
 
-    def get_decoder_feature_maps(self):
+    def get_decoder_features(self):
         return self.decoder_features
 
 
@@ -126,9 +108,10 @@ class IdtEncoder(nn.Module):
     def __init__(self, opt):
         self.model = torchvision.models.resnet101(
             num_classes=opt["num_classes"])
-        self.model.load_state_dict(
-            torch.load(opt["pretrained_model"], 
-            map_location=opt["map_location"]))
+
+        if opt["pretrained_model"] is not None:
+            self.model.load_state_dict(torch.load(opt["pretrained_model"], 
+                map_location=opt["map_location"]))
 
 
     def forward(self, x):
