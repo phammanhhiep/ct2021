a9ffe3516bd5b4c634453061a007051d1a4e5fa1
hieppm <hieppm.work@gmail.com>
2021-05-05 09:06:00 +0700

    Correct outputs of the 2 fc layers in AADNorm.forward; set default kernel_size, stride, and padding for convolution layers in AADNorm.__init__

--- src/models/networks/normalization.py
+++ src/models/networks/normalization.py
@@ -4,7 +4,9 @@ from torch import nn
 
 class AADNorm(nn.Module):
     def __init__(self, in_channels, attr_channels, idt_channels, opt):
-        """Summary
+        """
+        Every convolution layers are expected to not change spatial dimention of 
+        the corresponding input. 
         
         Args:
             in_channels (int): number of channels of the activation input from
@@ -12,40 +14,46 @@ class AADNorm(nn.Module):
             attr_channels (TYPE): Description
             opt (TYPE): Description
         """
-        super().__init()
+        super().__init__()
+        conv_kernel_size = 3
+        conv_stride = 1
+        conv_padding = 1
 
         self.bn = nn.BatchNorm2d(in_channels)
         self.mask_conv = nn.Conv2d(
-            in_channels,
-            in_channels,
-            opt["mask_conv"]["kernel_size"],            
-            )        
+            in_channels, in_channels, conv_kernel_size, conv_stride,
+            conv_padding)        
         self.sigmoid = nn.Sigmoid()
 
         self.gamma_conv = nn.Conv2d(
-            attr_channels,
-            in_channels,
-            opt["gamma_conv"]["kernel_size"],
+            attr_channels, in_channels, conv_kernel_size, conv_stride,
+            conv_padding
             )
         self.beta_conv = nn.Conv2d(
-            attr_channels,
-            in_channels,
-            opt["beta_conv"]["kernel_size"],
+            attr_channels, in_channels, conv_kernel_size, conv_stride,
+            conv_padding
             )
 
-        self.gamma_fc = nn.Linear(
-            idt_channels,
-            in_channels
-            )
-        self.beta_fc = nn.Linear(
-            idt_channels,
-            in_channels
-            )
+        self.gamma_fc = nn.Linear(idt_channels, in_channels)
+        self.beta_fc = nn.Linear(idt_channels, in_channels)
 
 
-    #TODO: consider construct output of beta_fc and gamma_fc large enough to able to reshape the output to the size of the activation input, instead of duplicating values with unsqueeze and expand_as; the current choice reduces the computational requirement, but at the same time duplicates values for each channel when being added to the h_hat, and thus may reduce the expressive power of the normalization
+    #TODO: the current implementation save memory and computation cost by duplicate output of beta_fc and gamma_fc; consider to remove the restriction.
     def forward(self, x):
+        """Spatial dimensions of each data point in attr and h are assumed to be
+        the same.
+        Note that after expand_as, output of beta_fc and gamma_fc are transformed
+        so that the final outputs having the same values in individual channels. 
+        Args:
+            x (TYPE): (bath of previous activations, batch of identity, 
+            batch of attributes). 
+        
+        Returns:
+            tensor: same size as input
+        """
         h, idt, attr = x
+        idt = idt.permute(0, 2, 3, 1)
+
         h_hat = self.bn(h)
         mask = self.sigmoid(self.mask_conv(h_hat))
 
@@ -53,10 +61,8 @@ class AADNorm(nn.Module):
         attr_gamma = self.gamma_conv(attr)
         denorm_attr =  h_hat * attr_gamma + attr_beta
 
-        idt_beta = self.beta_fc(idt).unsqueeze(-1).unsqueeze(-1).expand_as(
-            h_hat.size()) 
-        idt_gamma = self.gamma_fc(idt).unsqueeze(-1).unsqueeze(-1).expand_as(
-            h_hat.size())
+        idt_beta = self.beta_fc(idt).permute(0,3,1,2).expand_as(h) 
+        idt_gamma = self.gamma_fc(idt).permute(0,3,1,2).expand_as(h)
         denorm_idt = h_hat * idt_gamma + idt_beta
         
         return (1-mask) * denorm_attr + mask * denorm_idt
