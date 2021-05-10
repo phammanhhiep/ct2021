4ffb6eb63ecf7b7304d68e01c90f1072f957bdea
hieppm <hieppm.work@gmail.com>
2021-05-10 16:54:10 +0700

    Change according to change in dataset.py and faceShifter_trainer.py

--- src/train.py
+++ src/train.py
@@ -4,42 +4,35 @@ import logging
 from torch.utils import data
 
 
-from options.options import Options
-from trainers.faceShifter_trainer import FaceShifterTrainer
-from data.dataset import FFHQ, CelebAHQ
+from src.options.options import Options
+from src.trainers.faceShifter_trainer import FaceShifterTrainer
+from src.data.dataset import Dataset
 
 
+#TODO: explicit pass source and target image to trainer
 #TODO: remove the need to hard-code using if-else statement and specify datasets and trainers. Review SPADE project a hint. 
 #TODO: Consider using trainer from pytorch_lightning
+#TODO: Provide option to save model within each epoch
 def train():
     opt = Options()
-    dataset_name = opt["data"]["train"]
-    trainer_name = opt["trainer"]["name"] 
-
-    if dataset_name == "FFHQ":
-        train_datasets = FFHQ(opt[dataset_name]["train"])
-    elif dataset_name == "CelebAHQ":
-        train_datasets = CelebAHQ(opt[dataset_name]["train"])
+    trainer_name = opt["trainer"]["name"]
+    dataset_name = opt["dataset"]["train"]
+    
+    train_dataset = Dataset(opt[dataset_name]["train"])
     
     dataloader = data.DataLoader(
-        train_datasets, 
-        batch_size=opt["data"]["batch_size"],
+        train_dataset, 
+        batch_size=opt["dataset"]["batch_size"],
         shuffle=True,
-        num_workers=opt["data"]["num_worker"],
+        num_workers=opt["dataset"]["num_worker"],
         )
 
     if trainer_name == "FaceShifterTrainer"
-        trainer = FaceShifterTrainer(opt[trainer_name])
+        trainer = FaceShifterTrainer(opt)
 
-    for epoch in range(opt["trainer"]["epochs"]):
-        for j, batch_data in enumerate(dataloader):
-            trainer.fit(batch_data, j)
-        trainer.update_optimizer(epoch)
+    trainer.fit(dataloader)
 
-        if epoch % opt["checkpoint"]["save_interval"]
-            trainer.save_checkpoint(epoch, opt["checkpoint"]["save_dir"])
 
- 
 if __name__ == "__main__":
     create_root_logger()
     train()
