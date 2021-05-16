05e888f62e0b0566cfd981c9a2c661bda7d9eb10
hieppm <hieppm.work@gmail.com>
2021-05-16 17:45:09 +0700

    Ignore warning

--- train.sh
+++ train.sh
@@ -3,4 +3,4 @@
 export PYTHONPATH="$HOME/projects/ct2021"
 eval "$(conda shell.bash hook)"
 conda activate cv
-python src/train.py --option_file "train_options.yaml"
+python -W "ignore" src/train.py --option_file "train_options.yaml" 
