f481d19e4956658d36c25fd8426db7209d8ac1ef
hieppm <hieppm.work@gmail.com>
2021-05-17 22:38:40 +0700

    Allow to run the command from any directory

--- train.sh
+++ train.sh
@@ -3,4 +3,5 @@
 export PYTHONPATH="$HOME/projects/ct2021"
 eval "$(conda shell.bash hook)"
 conda activate cv
-python -W "ignore" src/train.py --option_file "train_options.yaml" 
+cd $HOME/projects/ct2021/
+python src/train.py --option_file "train_options.yaml"
