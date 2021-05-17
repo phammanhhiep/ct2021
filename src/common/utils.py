d1e34494e20bceb83ef49cb00779979adf8bb22a
hieppm <hieppm.work@gmail.com>
2021-05-17 10:54:30 +0700

    Implement load_state_dict and save_state_dict

--- src/common/utils.py
+++ src/common/utils.py
@@ -8,6 +8,9 @@ import argparse
 import torch
 
 
+logger = logging.getLogger(__name__)
+
+
 def create_root_logger(level=logging.DEBUG, file_name=None):
     FORMAT = logging.Formatter(
         "%(asctime)s — %(name)s — %(levelname)s — %(message)s")
@@ -38,8 +41,52 @@ def load_net(net, label, load_dir, device="cpu"):
     net.load_state_dict(torch.load(load_path, map_location=device))
 
 
-def save_model_from_checkpoint(pth, save_dir, device="cpu"):
-    """state dict of the generator from checkpoint
+def save_state_dict(state, name, save_dir, remove_old=True):
+    """Check if there exists file with the same name first, rename the old file,
+    save the file, and optionally remove the old file
+    
+    Args:
+        state (TYPE): Description
+        name (TYPE): Description
+        save_dir (TYPE): Description
+    """
+    save_path = os.path.join(save_dir, name)
+    old_path = save_path + ".old"
+    if os.path.exists(save_path):
+        os.rename(save_path, old_path)
+
+    torch.save(state, save_path)
+
+    while not os.path.exists(save_path):
+        time.sleep(2) # wait until the state is saved to the disk
+
+    if remove_old and os.path.exists(old_path):
+        os.remove(old_path)
+
+
+def load_state_dict(name, load_dir, device="cpu"):
+    """Summary
+    
+    Args:
+        name (TYPE): Description
+        load_dir (TYPE): Description
+        device (str, optional): Description
+    
+    Returns:
+        TYPE: Description
+    """
+    pth = os.path.join(load_dir, name)
+    state = None
+    try:
+        state = torch.load(pth, map_location=device)
+    except FileNotFoundError as e:
+        logger.error(str(e))
+    else:
+        return state
+
+
+def extract_model_from_checkpoint(pth, save_dir, device="cpu"):
+    """Extract the state dict of the generator from checkpoint, and save to disk
     
     Args:
         pth (TYPE): Description
