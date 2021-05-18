5abdef774f89ef148aa6c88cfd2ed4f5f3afe5d4
hieppm <hieppm.work@gmail.com>
2021-05-18 14:35:37 +0700

    Not update lastest checkpoint

--- train.sh
+++ train.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 
-checkpoint_dir=/mnt/disks/data1/ct2021_checkpoint/
-
-get_lastest_checkpoint() {
-    # Assume in the project directory
-    epoch=0
-    for name in $(ls "$checkpoint_dir" | grep "checkpoint" | grep -v "old"); do
-        new_epoch=$(echo $name | cut -d "." -f 1 | cut -d "_" -f 3)
-        if (( $epoch < $new_epoch )); then
-            epoch=$new_epoch
-        fi
-    done
-    echo "faceShifter_checkpoint_$epoch"
-}
-
-
 export PYTHONPATH="$HOME/projects/ct2021"
 eval "$(conda shell.bash hook)"
 conda activate cv
 cd $HOME/projects/ct2021/
-python src/train.py --option_file "train_options.yaml" --checkpoint $(get_lastest_checkpoint)
+python src/train.py --option_file "train_options.yaml"
