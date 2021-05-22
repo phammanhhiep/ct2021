82cbabf6e0d8da787bd5e3fb034e77fc41bac42a
hieppm <hieppm.work@gmail.com>
2021-05-22 11:34:11 +0700

    Define variables for all returned losses in fit_g

--- src/trainers/faceShifter_trainer.py
+++ src/trainers/faceShifter_trainer.py
@@ -111,8 +111,8 @@ class FaceShifterTrainer:
         y_attr = self.model.get_face_attribute(y)
         d_output = self.model(None, None, mode=3, x_hat=y)
 
-        loss = self.g_criterion(xt, y, xt_attr, y_attr, xs_idt, y_idt, d_output,
-            reconstructed)
+        adv_loss, attr_loss, rec_loss, idt_loss, loss = self.g_criterion(
+            xt, y, xt_attr, y_attr, xs_idt, y_idt, d_output,reconstructed)
         self.last_g_loss = loss.item()
         logger.info("adv_loss {}, attr_loss {}, rec_loss {}, idt_loss {}, \
             G_loss {}".format(adv_loss, attr_loss, rec_loss, idt_loss, loss))        
