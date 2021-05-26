c1505ace20922905bc971eeb0009549e7c3a5b64
hieppm <hieppm.work@gmail.com>
2021-05-26 08:25:26 +0700

    Remove prompt when install conda package

--- setup.sh
+++ setup.sh
@@ -5,6 +5,9 @@ req_file="requirements.txt"
 
 eval "$(conda shell.bash hook)"
 
+
+export CONDA_ALWAYS_YES="true"
 conda config --append channels conda-forge
 conda config --append channels pytorch
 conda create --name "$env_name" --file "$req_file"
+unset CONDA_ALWAYS_YES
