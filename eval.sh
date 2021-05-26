046dae3b2ff7432a10a357649f035b9299aed5b3
hieppm <hieppm.work@gmail.com>
2021-05-26 16:28:34 +0700

    Correct name of eval.py

--- eval.sh
+++ eval.sh
@@ -3,4 +3,4 @@
 export PYTHONPATH="$HOME/projects/ct2021"
 eval "$(conda shell.bash hook)"
 conda activate cv
-python src/evaluate.py --option_file "eval_options.yaml"
+python src/eval.py --option_file "eval_options.yaml"
