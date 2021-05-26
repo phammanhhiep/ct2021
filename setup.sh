2593836cc47b68ea2694e74b2db12b8b6171a65c
hieppm <hieppm.work@gmail.com>
2021-05-26 08:12:26 +0700

    Add conda channels

--- setup.sh
+++ setup.sh
@@ -4,4 +4,7 @@ env_name="cv"
 req_file="requirements.txt"
 
 eval "$(conda shell.bash hook)"
+
+conda config --append channels conda-forge
+conda config --append channels pytorch
 conda create --name "$env_name" --file "$req_file"
