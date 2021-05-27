bd30bd59a59861854f0957f7fa1f3d8b601056da
hieppm <hieppm.work@gmail.com>
2021-05-27 21:40:00 +0700

    Take command line option

--- eval.sh
+++ eval.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+options="eval_options.yaml"
+
+if [ ! -z "$1" ]; then
+    options="$1"
+fi
+
 export PYTHONPATH="$HOME/projects/ct2021"
 eval "$(conda shell.bash hook)"
 conda activate cv
-python src/eval.py --option_file "eval_options.yaml"
+python src/eval.py --option_file "$options"
