080d8dd368f7e453e53c7e150dddbd94df3e222c
hieppm <hieppm.work@gmail.com>
2021-05-30 09:09:44 +0700

    Take optional arguments and set default app dir, option path, and conda env

--- eval.sh
+++ eval.sh
@@ -1,25 +1,35 @@
 #!/bin/bash
 
-OPTIONS_FILE="eval_options.yaml"
-CONDA_ENV="ct2021"
+DEFAUL_OPTIONS_FILE="/mnt/disks/data/ct2021/options/eval_options.yaml"
+DEFAULT_CONDA_ENV="ct2021"
+DEFAULT_APP_DIR="$HOME/projects/ct2021"
 
 usage() {
     echo "Not implement yet"
 }
 
-
-while getopts 'e:o:h' c; do
+while getopts 'd:e:o:h' c; do
     case $c in
-        e) CONDA_ENV="$OPTARG" ;;
-        o) OPTIONS_FILE="$OPTARG" ;;
+        d) app_dir="$OPTARG"
+        e) myenv="$OPTARG" ;;
+        o) option_file="$OPTARG" ;;
         h) usage ;;
     esac
 done
 
+if [ -z "$myenv" ]; then
+    myenv="$DEFAULT_CONDA_ENV"
+fi
 
-export PYTHONPATH="$HOME/projects/ct2021"
+if [ -z "$option_file" ]; then
+    myenv="$DEFAUL_OPTIONS_FILE"
+fi
+
+if [ -z "$app_dir" ]; then
+    app_dir="$DEFAULT_APP_DIR"
+fi
+
+export PYTHONPATH="$app_dir"
 eval "$(conda shell.bash hook)"
-conda activate "$CONDA_ENV"
-python -W ignore src/eval.py --option_file "$OPTIONS_FILE"
-
-
+conda activate "$myenv"
+python -W ignore src/eval.py --option_file "$option_file"
