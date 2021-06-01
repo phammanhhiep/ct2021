86c2fea45003ed33e68b2ae4669540a81ddb3978
hieppm <hieppm.work@gmail.com>
2021-06-01 22:21:18 +0700

    Fix bug: ignore when same_file <= 0

--- src/data/data_list.py
+++ src/data/data_list.py
@@ -9,10 +9,9 @@ def create_data_list(data_names, distinct_pairs, same_files):
     data_names = np.random.permutation(data_names)
     distinct_files = int(distinct_pairs * 2)
     d = data_names[: distinct_files]
-    s = data_names[-int(same_files):]
-
     data_list = [[d[i],d[i+1],0] for i in range(0, distinct_files, 2)]
     if same_files > 0:
+        s = data_names[-int(same_files):]
         data_list += [[s[i],s[i],1] for i in range(same_files)]
     return data_list
 
