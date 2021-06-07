865590ef2c50e1d041d817e2e5e448512366da09
hieppm <hieppm.work@gmail.com>
2021-06-07 14:34:51 +0700

    Add comment

--- src/search_hyperparameters.py
+++ src/search_hyperparameters.py
@@ -6,4 +6,4 @@ from common.utils import create_root_logger
 
 logger = create_root_logger()
 
-# train multiple models (different in hyperarameters) 
+# train multiple models with different hyperarameters 
